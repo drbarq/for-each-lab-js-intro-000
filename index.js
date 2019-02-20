55ae638614ab6ba0eef86d2f9dbaf62da12c01fd
Joe Tustin <j.tustin@gmail.com>
2019-02-20 17:31:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,71 @@
+
+//- Define a function, `iterativeLog()`, that accepts an array. Call
+// `'forEach()` on this array, and inside the callback, log each element with the format `${index}: ${element}`.
+
+//var newArray = array;
+/*
+var array = [1,2,3];
+
+function iterativeLog(array) {
+  array.forEach((even, index, array) => {
+    console.log(`${index}: ${even}`)
+  }
+}
+*/
+
+function iterativeLog(array) {
+  array.foreach((array) => {
+    console.log(array)
+    //console.log(`${index}: ${even}`)
+  }
+);
+}
+
+/*
+```js
+var evens = [0, 2, 4, 6, 8, 10];
+
+function doToEvens(callback) {
+  evens.forEach(callback);
+}
+*/
+/*
+```js
+var evens = [0, 2, 4, 6, 8, 10];
+
+evens.forEach((even, index, array) => {
+  debugger;
+  console.log(`${even} is not odd!`);
+});
+```
+
+
+
+```js
+var evens = [0, 2, 4, 6, 8, 10];
+
+evens.forEach((even, index, array) => {
+  debugger;
+  console.log(`${even} is not odd!`);
+});
+```
+
+
+/*
+function callBack(evens, index, array) {
+  debugger;
+console.log(`${evens}: ${index});
+
+*/
+
+// console.log(newArray.next().value())
+
+//  console.log(array.slice());
+
+// Object.keys({'key': 'value'})
+  // `${index}: ${element}`)
+
+
+//  array = `${index}: ${element}`
+
+//  array.push(`${index}: ${element}`)
